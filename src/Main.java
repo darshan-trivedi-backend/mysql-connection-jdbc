f37429f0b0f5f02a46f884aac1a737b7aff900b1
Darshan.Trivedi <darshan.trivedi@DDTS-Macbook-Pro.local>
2024-01-18 18:54:07 +0530

    crud operation

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,4 @@
-import java.sql.DriverManager;
-import java.sql.Connection;
-import java.sql.Statement;
-
-class Student {
-    /*
+/*
      * Steps to connect your app with your db
      * 1. import package
      * 2. load & register
@@ -11,9 +6,28 @@ class Student {
      * 4. execute statement
      * 5. process the result
      * 6. close the connection
+     *
+     * student (id, name, email)
+     * mysql> desc student;
+       +-------+--------------+------+-----+---------+-------+
+       | Field | Type         | Null | Key | Default | Extra |
+       +-------+--------------+------+-----+---------+-------+
+       | id    | int          | YES  |     | NULL    |       |
+       | name  | varchar(200) | YES  |     | NULL    |       |
+       | email | varchar(200) | YES  |     | NULL    |       |
+       +-------+--------------+------+-----+---------+-------+
+       3 rows in set (0.01 sec)
+     *
+
      */
 
-    public void createDBConnection() {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+class Student {
+    public static void executeQuery(String query) {
         try {
             // Load the MySQL JDBC driver
             Class.forName("com.mysql.jdbc.Driver");
@@ -24,23 +38,85 @@ class Student {
 
             Connection connection = DriverManager.getConnection(url, username, password);
 
-
             Statement statement = connection.createStatement();
-            String query = "create table student (id int(3), name varchar(200), email varchar(200));";
 
-            boolean bl = statement.execute(query);
+            // Check if it's a SELECT query
+            if (query.trim().toLowerCase().startsWith("select")) {
+                ResultSet rs = statement.executeQuery(query);
 
+                while (rs.next()) {
+                    System.out.println("ID = " + rs.getInt(1));
+                    System.out.println("Name = " + rs.getString(2));
+                    System.out.println("Email = " + rs.getString(3));
+                    System.out.println("---------------");
+                }
+            } else {
+                boolean bl = statement.execute(query);
+
+                if (bl) {
+                    System.out.println(query + " Executed :)");
+                }
+            }
+
+            // Close resources
+            statement.close();
             connection.close();
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    public void createStudentTable() {
+        try {
+            String query = "create table if not exists student (id int(3), name varchar(200), email varchar(200));";
+            executeQuery(query);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void addStudent(int id, String name, String emailId) {
+        try {
+            String query = "insert into student(id, name, email) values (" + id + ", '" + name + "', '" + emailId + "')";
+            executeQuery(query);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void deleteStudent(int id) {
+        String query = "delete from student where id = " + id;
+        executeQuery(query);
+    }
+
+    public void getAllStudent() {
+        String query = "select * from student";
+        executeQuery(query);
+    }
+
+    public void getStudentById(int id) {
+        String query = "select * from student where id = " + id;
+        executeQuery(query);
+    }
+
+    public void updateStudent() {
+        String query = "update student set name = 'pratik' where id = 2";
+        executeQuery(query);
+    }
 }
 
 public class Main {
     public static void main(String[] args) {
         Student s = new Student();
-        s.createDBConnection();
+        s.createStudentTable();
+        s.getAllStudent();
+        s.addStudent(1, "Darshan", "dbt@gmail.com");
+        s.addStudent(2, "Dilip", "dks@gmail.com");
+        s.getStudentById(1);
+        s.updateStudent();
+        s.getAllStudent();
+        s.deleteStudent(2);
+        s.getAllStudent();
     }
 }
